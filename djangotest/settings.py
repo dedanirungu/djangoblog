e06ff054a3b8c1ecc6581ff524b6c65fc424768c
Dedan <dedanirungu@gmail.com>
2024-03-21 10:17:14 +0300

    Add User model import and update INSTALLED_APPS

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -38,11 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_tables2',
-    'django_filters'
+    'django_filters',
     'common',
     'blog',
-    'users',
-    
+
 ]
 
 MIDDLEWARE = [
